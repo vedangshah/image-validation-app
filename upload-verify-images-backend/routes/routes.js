e695c4926ad561b1149148aab5721bcb19b60ecb
Vedang Shah <vedangshah2301@gmail.com>
2020-10-15 16:01:26 +0530

    Add API Endpoint for Homepage

---
+++ upload-verify-images-backend/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+var router = express.Router();
+
+/* GET home page */
+router.get('/', function(req, res, next) {
+  res.json({title: 'Welcome to Upload Validate'});
+});
+
+module.exports = router;
