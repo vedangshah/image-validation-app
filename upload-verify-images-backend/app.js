88fe0fb6aa57312c65590de07a1cd65f3b5e2882
Vedang Shah <vedangshah2301@gmail.com>
2020-10-15 15:58:46 +0530

    Add the startup point for the app

---
+++ upload-verify-images-backend/app.js
@@ -0,0 +1,33 @@
+const _config = require('./config');
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/routes');
+
+var app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // send error to client or web server
+  res.status(err.status || 500);
+  res.json({message: err.message, error: err});
+});
+
+module.exports = app;
