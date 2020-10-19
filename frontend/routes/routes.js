54d95ce332c490d5ba296b6f5f99aa087f99c914
Vedang Shah <vedangshah2301@gmail.com>
2020-10-19 17:26:16 +0530

    Upload image to temporary storage

--- frontend/routes/routes.js
+++ frontend/routes/routes.js
@@ -1,8 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const request = require('request'); //importing Request - Simplified HTTP client package from npm to use it for sending POST request to API Server/Business Layer
+
 const express = require('express');
 var router = express.Router();
 
 const http = require('http'); //importing core http module to use it to send web layer requests to API server/Business Layer
 const endpointURL = 'http://localhost:3000'; //API Server/Business Layer endpoint URL
+const localImageDir = 'public/images/uploads/'; //Directory on web server where uploaded images will be stored temporarily
+
+//Using Multer middle-ware to upload image files from the form temporarliy on the web server before sending them to the API server/Business Layer
+const multer = require('multer');
+const storage = multer.diskStorage({
+    //destination is used to determine within which folder the uploaded files should be stored
+    destination: (req, file, cb) => {
+      cb(null, localImageDir)
+    },
+    //filename is used to determine what the file should be named inside the folder
+    filename: (req, file, cb) => {
+      let tempName = file.originalname;
+      tempName = tempName.replace(/\s+/g, '-').toLowerCase();
+      //checking if the file with the same name already exists or not
+      fs.access(localImageDir + tempName, fs.constants.F_OK, function(error) {
+        if (error) {
+          if(error.code === 'ENOENT') { //file does not exist
+            let finalFileName = tempName;
+            cb(null, finalFileName);
+          }
+          else {
+            console.error("Error checking existence of file in default directory:",error);
+          }
+        }
+        else { //file exists, rename file
+          let tempFileName = path.basename(localImageDir + tempName, path.extname(tempName)); //Stripping the file's extension
+          let finalFileName = tempFileName.concat("-" + Date.now() + path.extname(tempName));
+          cb(null, finalFileName);
+        }
+      })
+    }
+});
+const upload = multer({storage: storage});
 
 
 /* GET home page */
