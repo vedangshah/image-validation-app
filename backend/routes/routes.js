6c019eb79d822eaa0be1e9e49f9b1a10659b0e6e
Vedang Shah <vedangshah2301@gmail.com>
2020-10-19 16:53:55 +0530

    Show image upload page and setup its API

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.json({title: 'Welcome to Upload Validate'});
 });
 
+/* GET image upload page */
+router.get('/upload', function(req, res, next) {
+  res.json({title: 'Upload Image & Category', categories: [1,2,3,4,5,6,7,8,9,10]});
+});
+
 module.exports = router;
--- frontend/routes/routes.js
+++ frontend/routes/routes.js
@@ -33,4 +33,32 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* GET image upload page */
+router.get('/upload', function(req, res, next) {
+  var responseData = '';
+  //Using core http module to send request to and receive response from the API server/Business Layer
+  http.get(endpointURL.concat('/upload'), (response) => {
+    response.on('data', chunk => {
+      responseData += chunk;
+    });
+    response.on('end', () => {
+      try {
+        responseData = JSON.parse(responseData);
+        res.render('upload', responseData);
+      }
+      catch (error) {
+          console.error('Got error in parsing data:',error.message);
+          throw error;
+      }
+    });
+    response.on('error', (error) => {
+      console.log('Got error in response:',error.message);
+      throw error;
+    });
+  }).on('error', (error) => {
+      console.log('Got error in http/https.get():',error.message);
+      throw error;
+  });
+});
+
 module.exports = router;
